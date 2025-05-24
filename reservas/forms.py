81a7e75ae9db32096ce0f1fe1d6d53b1683c2275
Agustin <agustinayalaho@gmail.com>
2025-05-24 10:17:47 -0300

    Termine el formulario y algunas validaciones

--- reservas/forms.py
+++ reservas/forms.py
@@ -1,8 +1,24 @@
 from django import forms
-from .models import Reserva
+from .models import Reserva, Tarjeta
 from django.utils import timezone
 
 class ReservaForm(forms.ModelForm):
+
+    numero_tarjeta = forms.CharField(
+        label="Número de Tarjeta",
+        max_length=16,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    pin_tarjeta = forms.CharField(
+        label="PIN",
+        max_length=3,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    vencimiento_tarjeta = forms.DateField(
+        label="Fecha de Vencimiento",
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
+
     class Meta:
         model = Reserva
         fields = ['fecha_inicio', 'fecha_fin']
@@ -20,6 +36,9 @@ class ReservaForm(forms.ModelForm):
         cleaned_data = super().clean()
         fecha_inicio = cleaned_data.get('fecha_inicio')
         fecha_fin = cleaned_data.get('fecha_fin')
+        numero_tarjeta = cleaned_data.get('numero_tarjeta')
+        pin_tarjeta = cleaned_data.get('pin_tarjeta')
+        vencimiento_input = cleaned_data.get('vencimiento_tarjeta')
         
         if fecha_inicio and fecha_fin:
             # Validar que la fecha de inicio sea posterior a la fecha actual
@@ -42,6 +61,31 @@ class ReservaForm(forms.ModelForm):
                     self.add_error(None, f"El vehiculo ya esta reservado en el periodo seleccionado.")
                     break
         
+        
+         # Validación de tarjeta
+        try:
+            tarjeta = Tarjeta.objects.get(numero=numero_tarjeta)
+        except Tarjeta.DoesNotExist:
+            self.add_error('numero_tarjeta', "El número de tarjeta no es válido.")
+            return cleaned_data
+
+        if tarjeta.pin != pin_tarjeta:
+            self.add_error('pin_tarjeta', "El PIN ingresado no es correcto.")
+
+        if tarjeta.vencimiento != vencimiento_input:
+            self.add_error('vencimiento_tarjeta', "La fecha de vencimiento no coincide con la tarjeta.")
+
+        if tarjeta.vencimiento < timezone.now().date():
+            self.add_error('vencimiento_tarjeta', "La tarjeta está vencida.")
+
+        # Verificar saldo
+        if fecha_inicio and fecha_fin:
+            dias = (fecha_fin - fecha_inicio).days + 1
+            total = dias * self.vehiculo.precio_por_dia
+            if tarjeta.saldo < total:
+                self.add_error(None, "La tarjeta no tiene saldo suficiente para realizar la reserva.")
+
+
         return cleaned_data
 
 class CancelarReservaForm(forms.Form):
